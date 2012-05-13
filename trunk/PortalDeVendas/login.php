145f6da3d4d11c286532bb731b416fc6cb27a3c8
felipe.aug.silva@gmail.com <felipe.aug.silva@gmail.com@2033a7a8-0523-0eaf-e4fc-536b77912ab5>
2012-05-13 23:28:35 +0000

    Mudança de login.html para login.php. Criada estrutura para autenticar, falta obter WSDL e o nome do método.

---
+++ trunk/PortalDeVendas/login.php
@@ -0,0 +1,42 @@
+<?php
+if (isset($_POST['submit']))
+{
+    try
+    {
+        include("wsdl.php");
+
+        //$client = new SoapClient($wsdlComp07);
+
+        //$result = $client->NOME_DO_METODO($_POST['txtUser'], $_POST['txtPass']);
+
+        //if (sucesso) {
+            header('Location: categorias.php');
+        //} else {
+            //echo "<script language='javascript'>alert(\"Autenticacao falhou!\")</script>";
+        //}
+
+    } catch (Exception $e) {
+        echo "Exception: ";
+        echo $e->getMessage();
+    }
+}
+?>
+<html>
+    <body>
+        <form id="frmLogin" action="" method="post">
+            <table>
+                <tr>
+                    <td>Usuario: </td>
+                    <td><input id="txtUser" type="text"></td>
+                </tr>
+            <tr>
+                <td>Senha:</td>
+                <td><input name="txtPass" type="password>"></td>
+            </tr>
+            <tr>
+                <td><input name="submit" type="submit" value="Entrar"></td>
+            </tr>
+            <table>
+        </form>
+    </body>
+</html>
