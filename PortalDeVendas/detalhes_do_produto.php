47b5a4094d4cc05e6383d6a85e27652d69d261ec
felipe.aug.silva@gmail.com <felipe.aug.silva@gmail.com@2033a7a8-0523-0eaf-e4fc-536b77912ab5>
2012-05-13 17:09:12 +0000

    Mostrando informacoes de estoque e preco nos detalhes do produto. Criado arquivo wsdl.php com os endereços dos arquivos wsdl

--- PortalDeVendas/detalhes_do_produto.php
+++ PortalDeVendas/detalhes_do_produto.php
@@ -1,12 +1,26 @@
 <?php
 try {
-    $client = new SoapClient("http://sql2.students.ic.unicamp.br/~ra043251/mc747/DetalheProduto.wsdl");
-    $result = $client->exibeDetalhesID($_GET["prodID"]);
-    echo "<h3>".$result[1]."</h3>";
-    echo "<b>Marca: </b>".$result[5]."<br/>";
-    echo "<b>Especificacao: </b>".$result[6]."<br/>";
-    echo "<b>Peso: </b>".$result[7]." Kg<br/>";
-    echo "<b>Dimensoes: </b>".$result[8]." x ".$result[9]." x ".$result[10]." (comprimento x largura x altura)<br/><br/>";
+    include("wsdl.php");
+
+    $prodID = $_GET["prodID"];
+
+    // componente 01 - estoque    
+    $client = new SoapClient($wsdlComp01);
+    $resultComp01 = $client->ReturnProductInfo(array("ID" => "$prodID"));
+    
+    // componente 03 - informacoes produto
+    $client = new SoapClient($wsdlComp03);
+    $resultComp03 = $client->exibeDetalhesID($prodID);
+
+    // mostrar informacoes
+    echo "<h3>".$resultComp03[1]."</h3>";
+    echo "<b>Marca: </b>".$resultComp03[5]."<br/>";
+    echo "<b>Especificacao: </b>".$resultComp03[6]."<br/>";
+    echo "<b>Peso: </b>".$resultComp03[7]." Kg<br/>";
+    echo "<b>Dimensoes: </b>".$resultComp03[8]." x ".$resultComp03[9]." x ".$resultComp03[10]." (comprimento x largura x altura)<br/><br/>";
+    echo "<b>Preco: </b>R$ ".$resultComp01->ReturnProductInfoResult->Price."<br/>";
+    echo "<b>Quantidade em estoque: </b>".$resultComp01->ReturnProductInfoResult->Quantity."<br/><br/>";
+
 } catch (Exception $e) {
     echo "Exception: ";
     echo $e->getMessage();
--- PortalDeVendas/produtos_da_categoria.php
+++ PortalDeVendas/produtos_da_categoria.php
@@ -1,7 +1,10 @@
 <?php
 try {
-    $client = new SoapClient("http://sql2.students.ic.unicamp.br/~ra043251/mc747/DetalheProduto.wsdl");
+    include("wsdl.php");
+    
+    $client = new SoapClient($wsdlComp03);
     $result = $client->buscaAvancada($_GET["categID"], NULL);
+
     foreach ($result as $produto) {
         echo "<p><a href=\"detalhes_do_produto.php?prodID=".$produto[0]."\">".$produto[1]."</a></p>";
   	}
