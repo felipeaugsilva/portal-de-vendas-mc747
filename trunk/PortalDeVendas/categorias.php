53fb4034cd7293bab4fb50cfa24127cad64172b2
felipe.aug.silva@gmail.com <felipe.aug.silva@gmail.com@2033a7a8-0523-0eaf-e4fc-536b77912ab5>
2012-05-16 01:27:14 +0000

    Sessão autenticada

--- trunk/PortalDeVendas/categorias.php
+++ trunk/PortalDeVendas/categorias.php
@@ -1,15 +1,23 @@
 <?php
-try {
-    include("wsdl.php");
 
-    $client = new SoapClient($wsdlComp03);
-    $result = $client->listarCategorias();
+include("wsdl.php");
 
-    foreach ($result as $categoria) {
-        echo "<p><a href=\"produtos_da_categoria.php?categID=".$categoria[0]."\">".$categoria[2]."</a></p>";
-  	}
-} catch (Exception $e) {
-    echo "Exception: ";
-    echo $e->getMessage();
+session_start("sessao");
+
+if (!isset($_SESSION['cpf'])) {
+    header('Location: login.php');
+} else {
+    try {
+
+        $client = new SoapClient($wsdlComp03);
+        $result = $client->listarCategorias();
+
+        foreach ($result as $categoria) {
+            echo "<p><a href=\"produtos_da_categoria.php?categID=".$categoria[0]."\">".$categoria[2]."</a></p>";
+        }
+    } catch (Exception $e) {
+        echo "Exception: ";
+        echo $e->getMessage();
+    }
 }
 ?>
