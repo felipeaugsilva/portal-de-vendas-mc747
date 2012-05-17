35fb53284a89948a0d92681069b9cb523cdcd705
eyhiga@gmail.com <eyhiga@gmail.com@2033a7a8-0523-0eaf-e4fc-536b77912ab5>
2012-05-17 02:23:11 +0000

    Melhorando carrinho, agora com atualizacao de quantidades

--- trunk/PortalDeVendas/carrinho_de_compras.php
+++ trunk/PortalDeVendas/carrinho_de_compras.php
@@ -5,11 +5,11 @@ try {
 
     session_start("sessao");
 
-    /*if(!isset($_SESSION['cpf']))
+    if(!isset($_SESSION['cpf']))
     {
         header('Location: login.php');
     }
-    else*/
+    else
     {
         $action = $_GET["action"];
 
@@ -22,13 +22,15 @@ try {
 
             $prodID = $_GET["prodID"];
 
-            if(!$_SESSION["carrinho"][$prodID] || $_SESSION["carrinho"][$prodID] == "")
+            if(!$_SESSION["carrinho"][$prodID] 
+                || $_SESSION["carrinho"][$prodID] == "")
             {
                 $_SESSION["carrinho"][$prodID] = array();
 
                 // componente 01 - estoque    
                 $client = new SoapClient($wsdlComp01);
-                $resultComp01 = $client->ReturnProductInfo(array("ID" => "$prodID"));
+                $resultComp01 = $client->ReturnProductInfo(
+                    array("ID" => "$prodID"));
 
                 // componente 03 - informacoes produto
                 $client = new SoapClient($wsdlComp03);
@@ -37,74 +39,92 @@ try {
                 $_SESSION["carrinho"][$prodID]["id"] = $prodID;
                 $_SESSION["carrinho"][$prodID]["nome"] = $resultComp03[1];
                 $_SESSION["carrinho"][$prodID]["qtd"] = 1;
-                $_SESSION["carrinho"][$prodID]["preco"] = $resultComp01->ReturnProductInfoResult->Price;   
+                $_SESSION["carrinho"][$prodID]["preco"] = 
+                    $resultComp01->ReturnProductInfoResult->Price;   
             }
             else
             {
-                $_SESSION["carrinho"][$prodID]["qtd"] = intval($_SESSION["carrinho"][$prodID]["qtd"]) + 1;
+                $_SESSION["carrinho"][$prodID]["qtd"] = 
+                    intval($_SESSION["carrinho"][$prodID]["qtd"]) + 1;
             }
         }
         else if($action == "atualizar")
         {
-            foreach($_SESSION["carrinho"] as $produto)
+            $prodID = $_GET["prodID"];
+            $qtd = $_POST["qtd".$prodID];
+            
+            if(intval($qtd) > 0)
             {
-                $qtd = $_GET["qtd".$produto["id"]];
-
-                if(intval($qtd) > 0)
-                {
-                    $produto["qtd"] = $qtd;
-                }
-                else
-                {
-                    $produto = NULL;
-                }
+                $_SESSION["carrinho"][$prodID]["qtd"] = $qtd;
             }
+            else
+            {
+                unset($_SESSION["carrinho"][$prodID]);
+            }
+
         }
         else if($action == "frete")
         {
             $client = new SoapClient($wsdlComp09);
-            $resultComp09 = $client->ReturnProductInfo(array("ID" => "$prodID"));
+            $resultComp09 = $client->ReturnProductInfo(
+                array("ID" => "$prodID"));
         }
 
-        echo "<table>";
-        echo "<tr>";
-        echo "<td><b>Produto</b></td>";
-        echo "<td><b>Quantidade</b></td>";
-        echo "<td><b>Preco</b></td>";
-        echo "<td><b>Total</b></td>";
-        echo "<td><a href=\"carrinho_de_compras.php?action=atualizar\">Atualizar</a></td>";
-        echo "</tr>";
-
-        $total = intval(0);
-        foreach($_SESSION["carrinho"] as $produto)
+        if(!empty($_SESSION["carrinho"]))
         {
+            // echo "<form id=\"frmCarrinho\" action=\"\" method=\"post\">";
+            echo "<table>";
             echo "<tr>";
-            echo "<td>".$produto["id"]." - ".$produto["nome"]."</td>";
-            echo "<td><input id=\"qtd".$produto["id"]."\" name=\"qtd".$produto["id"]."\"type=\"text\" value=\"".$produto["qtd"]."\"></td>";
-            echo "<td>".$produto["preco"]."</td>";
-            $total_produto = intval($produto["qtd"]) * intval($produto["preco"]);
-            $total = intval($total) + intval($total_produto);
-            echo "<td>".$total_produto."</td>";
+            echo "<td><b>Produto</b></td>";
+            echo "<td><b>Quantidade</b></td>";
+            echo "<td><b>Preco</b></td>";
+            echo "<td><b>Total</b></td>";
+            //echo "<td><a href=\"carrinho_de_compras.php?action=atualizar\">Atualizar</a></td>";
+            echo "</tr>";
+
+            $total = intval(0);
+            foreach($_SESSION["carrinho"] as $produto)
+            {
+                echo "<tr>";
+                echo "<td>".$produto["id"]." - ".$produto["nome"]."</td>";
+                echo "<td>
+                    <form id=\"frmQtd".$produto["id"]."\" method=\"post\" action=\"carrinho_de_compras.php?action=atualizar&prodID=".$produto["id"]."\">
+                    <input id=\"qtd".$produto["id"]."\" name=\"qtd".$produto["id"]."\"type=\"text\" value=\"".$produto["qtd"]."\">
+                    <input type=\"submit\" id=\"btnQtd".$produto["id"]."\" value=\"Atualizar\">
+                    </form>
+                    </td>";
+                echo "<td>".$produto["preco"]."</td>";
+                $total_produto = intval($produto["qtd"]) * 
+                    intval($produto["preco"]);
+                $total = intval($total) + intval($total_produto);
+                echo "<td>".$total_produto."</td>";
+                echo "<tr>";
+            }
+
             echo "<tr>";
+            echo "<td></td>";
+            echo "<td></td>";
+            echo "<td><b>Total</b></td>";
+            echo "<td><b>".$total."</b></td>";
+            echo "</tr>";
+
+            echo "</table>";
+
+            echo "<table>";
+            echo "<tr>";
+            echo "<td>Calcular frete: </td>";
+            echo "<td><input id=\"cep\" type=\"text\" maxlength=\"8\"></td>";
+            echo "<td><a href=\"carrinho_de_compras.php?action=frete\">
+                Calcular</a></td>";
+            echo "<td></td>";
+            echo "</tr>";
+            echo "</table>";
+            //echo "</form>";
         }
-
-        echo "<tr>";
-        echo "<td></td>";
-        echo "<td></td>";
-        echo "<td><b>Total</b></td>";
-        echo "<td><b>".$total."</b></td>";
-        echo "</tr>";
-
-        echo "</table>";
-
-        echo "<table>";
-        echo "<tr>";
-        echo "<td>Calcular frete: </td>";
-        echo "<td><input id=\"cep\" type=\"text\" maxlength=\"8\"></td>";
-        echo "<td><a href=\"carrinho_de_compras.php?action=frete\">Calcular</a></td>";
-        echo "<td></td>";
-        echo "</tr>";
-        echo "</table>";
+        else
+        {
+            echo "Carrinho vazio";
+        }
     }
 
 } catch (Exception $e) {
