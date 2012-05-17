eb74683c8ecfced7427cf56b968d4d826c71ffde
eyhiga@gmail.com <eyhiga@gmail.com@2033a7a8-0523-0eaf-e4fc-536b77912ab5>
2012-05-17 22:09:16 +0000

    Modificando carrinho

--- trunk/PortalDeVendas/wsdl.php
+++ trunk/PortalDeVendas/wsdl.php
@@ -4,7 +4,7 @@
     $wsdlComp03 = "http://sql2.students.ic.unicamp.br/~ra043251/mc747/DetalheProduto.wsdl";
     $wsdlComp04 = "";
     $wsdlComp05 = "";
-    $wsdlComp06 = "";
+    $wsdlComp06 = "http://staff01.lab.ic.unicamp.br:8680/c06/services/C06_Logistica?wsdl";
     $wsdlComp07 = "";
     $wsdlComp08 = "http://mc747atendimento.no-ip.org:2121/AtendimentoCliente.AtendimentoCliente.svc?wsdl";
     $wsdlComp09 = "http://www.criativosilimitada.com.br/jorge/mc747/address-service.php?wsdl";
