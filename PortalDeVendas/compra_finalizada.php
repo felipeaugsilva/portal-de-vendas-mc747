a6ced22bcad8fe992a7482a2373990444a54c7ff
felipe.aug.silva@gmail.com <felipe.aug.silva@gmail.com@2033a7a8-0523-0eaf-e4fc-536b77912ab5>
2012-05-20 20:13:23 +0000

    Adicionado link para voltar para Home depois de finalizar a compra

--- PortalDeVendas/compra_finalizada.php
+++ PortalDeVendas/compra_finalizada.php
@@ -1,3 +1,6 @@
-<?php
-echo "<h3>Compra finalizada com sucesso!</h3>";
-?>
+<html>
+<body>
+    <h2>Compra finalizada com sucesso!</h2>
+    <p><a href="index.php">Voltar para Home Page</a></p>
+</body>
+</html>
