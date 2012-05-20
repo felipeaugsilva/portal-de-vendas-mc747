fd5ea3ccc8992a996fd307cfe3bf42b5ebeb0fc3
felipe.aug.silva@gmail.com <felipe.aug.silva@gmail.com@2033a7a8-0523-0eaf-e4fc-536b77912ab5>
2012-05-20 18:48:56 +0000

    Atualizando chamados para listar todos do cliente em vez de pedir o ID do chamado

--- PortalDeVendas/chamados.php
+++ PortalDeVendas/chamados.php
@@ -1,7 +1,34 @@
-<html>
-<body>
-    <h2>Chamados</h2>
-    <p><a href="abrir_chamado">Abrir novo chamado</a></p>
-    <p><a href="consultar_chamado">Consultar chamado</a></p>
-</body>
-</html>
+<?php
+
+include("wsdl.php");
+
+try
+{
+    echo "<h2>Chamados</h2>";
+    
+    $client = new SoapClient($wsdlComp08);
+
+    $cpf = "7";     // TODO pegar da sessao
+    
+    $args = array ( "idCliente"     => "00000000-0000-0000-0000-000000000004",
+                    "idUsuario"     => $cpf,
+                    "tipoChamado"   => NULL );
+    
+    $result = $client->Consultar_Chamados_Por_Usuario($args);
+    
+    echo "<table>";
+    foreach ($result->Consultar_Chamados_Por_UsuarioResult->ChamadoResumido as $row) {
+        echo "<tr>";
+        echo "<td><a href=\"consultar_chamado.php?idChamado=".$row->Id."\">".$row->Id."</td>";
+        echo "<td>".$row->Descricao."</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+    
+    echo "<p><a href=\"abrir_chamado.php\">Abrir novo chamado</a></p><br/>";
+
+} catch (Exception $e) {
+    echo "Exception: ";
+    echo $e->getMessage();
+}
+?>
--- PortalDeVendas/consultar_chamado.php
+++ PortalDeVendas/consultar_chamado.php
@@ -4,85 +4,60 @@ include("wsdl.php");
 
 $tipos = array("Reclamacao", "Sugestao", "Troca", "Duvida", "Outro");
 
-if (isset($_POST['submit']))
+try
 {
-    try
-    {
-        $client = new SoapClient($wsdlComp08);
-        
-        $args = array( "idCliente" => "00000000-0000-0000-0000-000000000004",
-                       "idChamado" => $_POST['idChamado'] );
-        
-        $result = $client->Consultar_Chamado($args);
+    $client = new SoapClient($wsdlComp08);
+    
+    $args = array( "idCliente" => "00000000-0000-0000-0000-000000000004",
+                   "idChamado" => $_GET['idChamado'] );
+    
+    $result = $client->Consultar_Chamado($args);
 ?>
-        
-        <h3>Detalhes do chamado</h3>
-        <table>
-            <tr>
-                <td><b>ID do chamado:</b></td>
-                <td><?php echo $result->Consultar_ChamadoResult->Id; ?></td>
-            </tr>
-            <tr>
-                <td><b>Chamado aberto em:</b></td>
-                <td><?php echo $result->Consultar_ChamadoResult->Data; ?></td>
-            </tr>
-            <tr>
-                <td><b>Descricao:</b></td>
-                <td><?php echo $result->Consultar_ChamadoResult->Descricao; ?></td>
-            </tr>
-            <tr>
-                <td><b>Tipo do chamado:</b></td>
-                <td><?php echo $tipos[$result->Consultar_ChamadoResult->TipoChamado]; ?></td>
-            </tr>
-            <tr>
-                <td><b>ID do pedido:</b></td>
-                <td><?php echo $result->Consultar_ChamadoResult->IdPedido; ?></td>
-            </tr>
-            <tr>
-                <td><b>ID do produto:</b></td>
-                <td><?php echo $result->Consultar_ChamadoResult->IdProduto; ?></td>
-            </tr>
-            <tr>
-                <td><b>Alteracoes:</b></td>
-                <td><?php
-                    foreach ($result->Consultar_ChamadoResult->Alteracoes->Alteracao as $row) {
-                        echo "<b>Data: </b>".$row->Data."<br/>";
-                        echo "<b>Descricao: </b>".$row->Descricao."<br/>";
-                        echo "<b>Id: </b>".$row->Id."<br/><br/>";
-                    }
-                ?></td>
-            </tr>
-        <table>
-        
-        <p><a href="consultar_chamado.php">Consultar outro chamado</a></p>
-        <p><a href="chamados.php">Ir para chamados</a></p>
-<?php        
+    
+    <h2>Detalhes do chamado</h2>
+    <table>
+        <tr>
+            <td><b>ID do chamado:</b></td>
+            <td><?php echo $result->Consultar_ChamadoResult->Id; ?></td>
+        </tr>
+        <tr>
+            <td><b>Chamado aberto em:</b></td>
+            <td><?php echo $result->Consultar_ChamadoResult->Data; ?></td>
+        </tr>
+        <tr>
+            <td><b>Descricao:</b></td>
+            <td><?php echo $result->Consultar_ChamadoResult->Descricao; ?></td>
+        </tr>
+        <tr>
+            <td><b>Tipo do chamado:</b></td>
+            <td><?php echo $tipos[$result->Consultar_ChamadoResult->TipoChamado]; ?></td>
+        </tr>
+        <tr>
+            <td><b>ID do pedido:</b></td>
+            <td><?php echo $result->Consultar_ChamadoResult->IdPedido; ?></td>
+        </tr>
+        <tr>
+            <td><b>ID do produto:</b></td>
+            <td><?php echo $result->Consultar_ChamadoResult->IdProduto; ?></td>
+        </tr>
+        <tr>
+            <td><b>Alteracoes:</b></td>
+            <td><?php
+                foreach ($result->Consultar_ChamadoResult->Alteracoes->Alteracao as $row) {
+                    echo "<b>Data: </b>".$row->Data."<br/>";
+                    echo "<b>Descricao: </b>".$row->Descricao."<br/>";
+                    echo "<b>Id: </b>".$row->Id."<br/><br/>";
+                }
+            ?></td>
+        </tr>
+    <table>
+    
+    <p><a href="chamados.php">Consultar outro chamado</a></p>
+
+<?php
 
     } catch (Exception $e) {
         echo "Exception: ";
         echo $e->getMessage();
     }
-}
-else {
-?>
-
-<html>
-<body>
-    <h3>Consultar Chamado</h3>
-    <form name="consultarChamado" action="" method="post">
-        <table>
-            <tr>
-                <td>ID do chamado:</td>
-                <td><input name="idChamado" type="text"></td>
-            </tr>
-            <tr>
-                <td><input type="submit" name="submit" value="Enviar"></td>
-            </tr>
-        <table>
-    </form>
-</body>
-</html>
-
-<?php
-}
 ?>
