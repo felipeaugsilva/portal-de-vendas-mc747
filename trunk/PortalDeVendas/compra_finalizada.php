612e76bbd00a67656ff0a4e4afd32c8c540455fb
felipe.aug.silva@gmail.com <felipe.aug.silva@gmail.com@2033a7a8-0523-0eaf-e4fc-536b77912ab5>
2012-05-31 22:28:13 +0000

    Mostrando ID do pagamento ao final da compra

--- trunk/PortalDeVendas/compra_finalizada.php
+++ trunk/PortalDeVendas/compra_finalizada.php
@@ -1,6 +1,16 @@
-<html>
-<body>
-    <h2>Compra finalizada com sucesso!</h2>
-    <p><a href="index.php">Voltar para Home Page</a></p>
-</body>
-</html>
+<?php
+
+session_start("sessao");
+
+if (is_null($_SESSION['cpf']) || empty($_SESSION['cpf']) || !isset($_SESSION['cpf'])) {
+    header('Location: login.php');
+}
+
+echo "<h2>Compra finalizada com sucesso!</h2>";
+if (isset($_SESSION["idPagamento"])) {
+    echo "<p><b>ID entrega: </b>".$_SESSION["idPagamento"]."</p>";
+    unset($_SESSION["idPagamento"]);
+}
+echo "<p><a href=\"index.php\">Voltar para Home Page</a></p>";
+
+?>
